faaa8f6909c5c454cbcc1ff2fd2f9c904a2adabe
Seonghyeon Moon <shine7329@gmail.com>
2022-03-13 17:32:07 +0900

    프로젝트 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-function App() {
-	return <></>;
-}
+import { Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+
+const App = () => {
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={<Main />} />
+			</Routes>
+		</>
+	);
+};
 
 export default App;
---
+++ src/pages/Main.tsx
@@ -0,0 +1,5 @@
+const Main = () => {
+  return <h2>Main</h2>
+}
+
+export default Main
