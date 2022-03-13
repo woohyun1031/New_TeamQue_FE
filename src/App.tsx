1fc5b27d33a14d2b72e5102fd096b1a760d48fc4
Seonghyeon Moon <shine7329@gmail.com>
2022-03-13 18:01:01 +0900

    Styled components 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
 import Main from './pages/Main';
 
 const App = () => {
 	return (
 		<>
+			<Header />
 			<Routes>
 				<Route path='/' element={<Main />} />
 			</Routes>
