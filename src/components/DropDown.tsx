6208e77a5a7917544568e03ed5deb4c0bc51ee6b
Seonghyeon Moon <shine7329@gmail.com>
2022-03-28 01:42:53 +0900

    Feat: DropDown 로직 모달로 변경

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -17,73 +17,89 @@ const DropDown: React.FC<Props> = ({ name }) => {
 		dispatch(signOut());
 		location.reload();
 	};
+	const openMypage = () => {
+		alert('마이페이지 구현 중');
+	};
 	return (
 		<Container>
-			<Name onClick={toggleDropDown}>{name} 님</Name>
-			<Menu isOpen={isOpen}>
-				<li>마이페이지</li>
-				<li onClick={logout}>로그아웃</li>
-				<Line />
-			</Menu>
+			<Name onClick={toggleDropDown}>
+				{name} 님 
+				<ArrowIcon
+					src={isOpen ? '/images/arrowUp.png' : '/images/arrowDown.png'}
+				/>
+			</Name>
+			{isOpen && (
+				<>
+					<BackGround onClick={toggleDropDown} />
+					<Menu>
+						<li onClick={openMypage}>마이페이지</li>
+						<li onClick={logout}>로그아웃</li>
+					</Menu>
+				</>
+			)}
 		</Container>
 	);
 };
 
 export default DropDown;
 
-
-
-
-interface MenuProps {
-	isOpen: boolean;
-}
-
-const Menu = styled.ul<MenuProps>`
-	/* ${(props) => !props.isOpen && 'opacity: 0;'} */
-	/* opacity: 0; */
-	/* 사이즈 */
-	width: 80px;
-	height: 65px;
-	/* 레이아웃 */
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: space-around;
-	/* 스타일 */
-	font-weight: 400;
-	position: absolute;
-	bottom: -60px;
-	right: -15px;
-	font-size: 13px;
-	border-radius: 7px;
-	box-shadow: 0 2px 2px rgba(0, 0, 0, 0.25);
-	transition: 0.3s;
-	& li:nth-child(2) {
-		color: #718aff;
-		cursor: pointer;
-	}
-	display: none;
+const Container = styled.div`
+	position: relative;
 `;
 
 const Name = styled.button`
 	border: none;
 	background: none;
-	/* 임시 스타일 */
 	font-size: 14px;
 	font-weight: 600;
-`;
-
-const Container = styled.div`
+	cursor: pointer;
 	position: relative;
-	padding-bottom: 20px ;
-	&:hover ${Menu} {
-		display: flex;
-	}
+	z-index: 101;
 `;
 
-const Line = styled.div`
-	width: 70px;
-	height: 1px;
-	border-bottom: 1px solid #d2d2d2;
-	position: absolute;
+const ArrowIcon = styled.div<{ src: string }>`
+	display: inline-block;
+	width: 14px;
+	height: 8px;
+	margin-left: 5px;
+	background-image: url(${({ src }) => src});
+	background-repeat: no-repeat;
+	background-position: center center;
 `;
+
+const Menu = styled.ul`
+	width: 80px;
+	height: 65px;
+	border-radius: 7px;
+	padding: 5px;
+	font-size: 13px;
+	font-weight: 400;
+	bottom: -70px;
+	left: -6px;
+	box-shadow: 0 2px 2px rgba(0, 0, 0, 0.25);
+	background-color: #fff;
+	& li {
+		width: 70px;
+		text-align: center;
+		margin: 0 auto;
+		cursor: pointer;
+		padding: 4px 0;
+	}
+	& li:nth-child(2) {
+		color: #718aff;
+		border-top: 1px solid #D2D2D2;
+	}
+	position: absolute;
+	z-index: 101;
+`;
+
+
+const BackGround = styled.div`
+	position: fixed;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	top: 0;
+	z-index: 100;
+`;
+
