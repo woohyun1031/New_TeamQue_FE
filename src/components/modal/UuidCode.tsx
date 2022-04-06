dbbc9706cf462e7fecd8a7dcf3cf2d18b3912989
Woohyun <ktkwhms3@gmail.com>
2022-04-07 04:10:34 +0900

    Refactor: uuid component name => StreamKey 변경

--- src/components/modal/UuidCode.tsx
+++ src/components/modal/UuidCode.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useEffect, useRef, useState } from 'react';
+import { MouseEvent } from 'react';
 import { useQuery } from 'react-query';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
@@ -6,7 +6,7 @@ import api from '../../api';
 import { RootState } from '../../store/configStore';
 import ModalCloseButton from './ModalCloseButton';
 
-const UuidCode = () => {
+const StreamKey = () => {
 	const classid: string = useSelector((state: RootState) => state.modal.data);
 
 	const { data } = useQuery('streamKey', () =>
@@ -21,7 +21,6 @@ const UuidCode = () => {
 			});
 		}
 	};
-	console.log(data);
 	return (
 		<Form>
 			<ModalCloseButton />
@@ -37,7 +36,7 @@ const UuidCode = () => {
 	);
 };
 
-export default UuidCode;
+export default StreamKey;
 
 const Form = styled.form`
 	display: flex;
